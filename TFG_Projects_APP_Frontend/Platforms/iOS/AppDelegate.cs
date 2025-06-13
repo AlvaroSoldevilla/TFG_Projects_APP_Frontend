173a32d69019597209c6586873209be782ae2386
Alvaro Soldevilla <124153203+AlvaroSoldevilla@users.noreply.github.com>
2025-06-13 17:35:25 +0200

    V1.0 - Fixed issues detected in SAST test. Recompiled the app.

--- TFG_Projects_APP_Frontend/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace TFG_Projects_APP_Frontend;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
--- TFG_Projects_APP_Frontend/Services/UsersService/UserSession.cs
+++ TFG_Projects_APP_Frontend/Services/UsersService/UserSession.cs
@@ -6,5 +6,5 @@ namespace TFG_Projects_APP_Frontend.Services.UsersService;
 public class UserSession
 {
     public AppUser User { get; set; } = new() { Id = 1, Username = "Admin", Email = "admin@test.com"};
-    public string Token { get; set; } = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWRtaW5AdGVzdC5jb20iLCJleHBpcmVzIjoxNzQ5Mjk1MTczLjc3MTcyMzV9.WRrHKpL-28lKdo-aE2ctgYU4_RW0CTAK52G3Rk8RJK0";
+    public string Token { get; set; } = "";
 }
